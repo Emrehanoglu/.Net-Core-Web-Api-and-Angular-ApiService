4df8a41ff31bb698d0e1fd0be3769ea78a13b7aa
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-15 04:23:40 +0300

    ServerApp #7

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ServerApp.Data;
 using ServerApp.Models;
 
 namespace ServerApp.Controllers
@@ -14,37 +16,32 @@ namespace ServerApp.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private static List<Product> _products;
-        public ProductsController()
+        private readonly SocialContext _socialContext;
+        public ProductsController(SocialContext socialContext)
         {
-            _products = new List<Product>();
-            _products.Add(new Product{ProductId=1,Name="Samsung S6", Price=3000, IsActive=false});
-            _products.Add(new Product{ProductId=2,Name="Samsung S7", Price=4000, IsActive=true});
-            _products.Add(new Product{ProductId=3,Name="Samsung S8", Price=5000, IsActive=true});
-            _products.Add(new Product{ProductId=4,Name="Samsung S9", Price=6000, IsActive=false});
-            _products.Add(new Product{ProductId=5,Name="Samsung S10", Price=7000, IsActive=true});
+            _socialContext = socialContext;
         } 
-        //yukarıdaki Products bilgilerini sanki veritabanından getirir
-        //gibi getireceğim.
+
         //localhost:5000/api/Products
         [HttpGet]
-        public List<Product> GetProducts(){
-            return _products;
+        public async Task<ActionResult> GetProducts(){
+            var products = await _socialContext.Products.ToListAsync();
+            return Ok(products);
         }
 
-        //localhost:5000/api/Products/2 --> 2. index 'deki elemanı getirecek
+        //localhost:5000/api/Products/2 --> 2. id 'deki elemanı getirecek
         [HttpGet("{id}")]
-        public IActionResult GetProduct(int id){
-            var p = _products.FirstOrDefault(x=>x.ProductId == id);
+        public async Task<ActionResult> GetProduct(int id){
+            var p = await _socialContext.Products.FirstOrDefaultAsync(x=>x.ProductId == id);
             if(p==null)
                 return NotFound();
             return Ok(p);
         }
 
-        [HttpPost]
-        public IActionResult CreateProduct(Product p){
-            _products.Add(p); //Server tarafına ürün eklendi.
-            return Ok(p); //eklediğim ürün bilgisini Client tarafına gönderdim.
-        }
+        // [HttpPost]
+        // public IActionResult CreateProduct(Product p){
+        //     _products.Add(p); //Server tarafına ürün eklendi.
+        //     return Ok(p); //eklediğim ürün bilgisini Client tarafına gönderdim.
+        // }
     }
 }
