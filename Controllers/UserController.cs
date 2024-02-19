b43a4908ea3f22831e4c69d3e85d2896d6a0e686
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-19 09:01:02 +0300

    ServerApp #17

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,15 +13,17 @@ namespace ServerApp.Controllers
     [Route("api/[controller]")]
     public class UserController:ControllerBase
     {
-        private UserManager<User> _userManager;
-        public UserController(UserManager<User> userManager)
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public UserController(UserManager<User> userManager,SignInManager<User> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         //http://localhost:5000/api/user/register
         [HttpPost("register")]
-        public async Task<IActionResult> Register(UserDTO model){
+        public async Task<IActionResult> Register(UserForRegisterDTO model){
             var user = new User{
                 UserName = model.UserName,
                 Email = model.Email,
@@ -36,6 +38,32 @@ namespace ServerApp.Controllers
                 return StatusCode(201);
             }
             return BadRequest(result.Errors);
+        }
+
+        //http://localhost:5000/api/user/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO model){
+            //model üzerinden UserName bilgisi alındığı için,
+            //bakalım gercekten boyle bir kullanıcı var mı? 
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if(user==null){
+                return BadRequest(new { message = "username is not exists"});
+            }
+
+            //username bilgisi ile parolayı kontrol ediyorum
+            //bunun için SignInManager gerekiyor
+            //lockoutOnFailure:false ile Startup.cs içerisindeki konf. ezerek hesap kitlenmesin diyorum
+            var result = await _signInManager.CheckPasswordSignInAsync(user,model.Password,lockoutOnFailure:false);
+            if(result.Succeeded){
+                //login
+                return Ok(new {
+                    token  = "token",
+                    username = user.UserName
+                });
+            }
+
+            return Unauthorized();
         }
     }
 }
--- DTO/UserForRegisterDTO.cs
+++ DTO/UserForRegisterDTO.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ServerApp.DTO
 {
-    public class UserDTO
+    public class UserForRegisterDTO
     {
+        [Required]
         public string Name { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
